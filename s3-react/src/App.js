f7831687a639756cc10078270f5e240c3264b124
Kingsley Ijomah <git-example@codehance>
2022-08-31 08:20:11 -0400

    Initial commit

---
+++ s3-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>S3 Project</h1>
+      <p>yah! it worked!!!</p>
+    </div>
+  );
+}
+
+export default App;
